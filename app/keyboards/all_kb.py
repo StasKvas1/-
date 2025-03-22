90fa092851be6e9a0fafbbec54d5a42411a39749
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2025-03-22 15:13:24 +0300

    v.0.0.0.2

--- app/keyboards/all_kb.py
+++ app/keyboards/all_kb.py
@@ -3,8 +3,7 @@ from create_bot import admins
 
 def main_kb(user_telegram_id: int):
     kb_list = [[KeyboardButton(text='о нас'),
-                KeyboardButton(text='профиль')]
-
+                KeyboardButton(text='профиль')],
                 [KeyboardButton(text='настроики'),
                 KeyboardButton(text='каталог')]]
     keyboard = ReplyKeyboardMarkup(
@@ -18,13 +17,6 @@ def main_kb(user_telegram_id: int):
     return keyboard
 
 def create_spec_kb():
-      keyboard = ReplyKeyboardMarkup(
-                keyboard=kb_list,
-                resize_keyboard=True,
-                one_time_keyboard=True,
-                input_field_placeholder='Воспользуитесь меню:'
-
-            )
       kb_list=[
             [
                   KeyboardButton(text='геопозиция',
@@ -35,3 +27,12 @@ def create_spec_kb():
                                  request_contact=True)
             ],
       ]
+      
+      keyboard = ReplyKeyboardMarkup(
+                keyboard=kb_list,
+                resize_keyboard=True,
+                one_time_keyboard=True,
+                input_field_placeholder='Воспользуитесь меню:'
+                )
+      
+      return keyboard
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 from create_bot import bot, dp
-from app.handlers.start_touter import start_router
+from stas.app.handlers.start_router import start_router
 
 async def main():
     dp.include_router(start_router)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.stert_polling(bot)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
